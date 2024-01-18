1a58eb89af4bbe969bd304932e61b05e05890272
Ricardo Jesus <ricardojesus189@gmail.com>
2024-01-18 18:25:53 +0000

    Cenas minhas

---
+++ DataLayer/SqlDataAccess.cs
@@ -0,0 +1,50 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
+namespace DataLayer;
+
+public class SqlDataAccess : ISqlDataAccess
+{
+    private readonly IConfiguration _config;
+    public string ConnectionStringName { get; set; } = "Default";
+    public SqlDataAccess(IConfiguration config)
+    {
+        _config = config;
+    }
+    public async Task<List<T>> LoadData<T, U>(string sql, U parameters)
+    {
+        try
+        {
+            Console.WriteLine("[Antes de _cfg.GetConnString]Entrei em LoadData: " + sql);
+            string? connectionString = _config.GetConnectionString(ConnectionStringName);
+            Console.WriteLine("[Depois de string? connectionString][antes de using IDBConnection]");
+            Console.WriteLine($"Connection string: {connectionString}");
+            using IDbConnection connection = new SqlConnection(connectionString);
+            Console.WriteLine("[Depois de IDbConnection connection][antes de var data = await connection]");
+            var data = await connection.QueryAsync<T>(sql, parameters);
+            Console.WriteLine("Antes do return de LaodDta em SQLDataAccess!");
+            return data.ToList();
+        } catch(Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+            return new List<T>();
+        }
+    }
+    public async Task SaveData<T>(string sql, T parameters)
+    {
+        string? connectionString = _config.GetConnectionString(ConnectionStringName);
+        using (IDbConnection connection = new SqlConnection(connectionString))
+        {
+            await connection.ExecuteAsync(sql, parameters);
+        }
+    }
+
+    public async void ExecuteData<T>(string sql, T parameters)
+    {
+        string? connectionString = _config.GetConnectionString(ConnectionStringName);
+        using IDbConnection connection = new SqlConnection(connectionString);
+        await connection.ExecuteAsync(sql, parameters);
+    }
+}
---
+++ DataLayer/Utilizador/UtilizadorRepository.cs
@@ -0,0 +1,53 @@
+﻿namespace DataLayer.Utilizador;
+
+public class UtilizadorRepository : IUtilizadorRepository
+{
+    private ISqlDataAccess _db; 
+    public UtilizadorRepository(ISqlDataAccess db)
+    {
+        _db = db;
+    }
+
+    public UtilizadorModel Find(string username)
+    {
+        Console.WriteLine($"Entrei em Find Utilizador Movel com username: {username}!");
+        string sql = $"select * from Utilizador where idUtilizador = {username}";
+        List<UtilizadorModel> utiRes = _db.LoadData<UtilizadorModel, dynamic>(sql, new { }).Result;
+        Console.WriteLine("depois de _db.WriteData");
+        if(utiRes != null) { Console.WriteLine("Encontrei "+ utiRes.Count); }
+        else { Console.WriteLine("Utilizador encontrou null");  }
+        return utiRes.First();
+    }
+
+    public Task<List<UtilizadorModel>> FindAll()
+    {
+        string sql = "select * from Utilizador";
+        return _db.LoadData<UtilizadorModel, dynamic>(sql, new { });
+    }
+
+    public async Task Create(UtilizadorModel user)
+    {
+        string sql = "INSERT INTO Utilizador (username, password, avaliacaoMedia, numeroDeLeiloesFeitos, dataDeRegisto)" +
+                     "VALUES (@username, @password, @avaliacaoMedia, @numeroDeLeiloesFeitos, @dataDeRegisto)";
+
+        await _db.SaveData(sql, new 
+        {
+            user.username,
+            user.password,
+            user.avaliacaoMedia,
+            user.numeroDeLeiloesFeitos,
+            user.dataDeRegisto
+        });
+    }
+
+    public Task<UtilizadorModel> Update(UtilizadorModel user)
+    {
+        //string sql = "UPDATE Utilizador SET ";
+        throw new NotImplementedException();
+    }
+
+    public Task Remove(int code)
+    {
+        throw new NotImplementedException();
+    }
+}
