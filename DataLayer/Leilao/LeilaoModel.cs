6e481d03a1dabd01c37695c3cedaf2f5ef6dba75
Rui Pinto <94129877+RuiPintoUM@users.noreply.github.com>
2024-01-19 15:48:41 +0000

    LicitacaoAtual

--- DataLayer/Leilao/LeilaoModel.cs
+++ DataLayer/Leilao/LeilaoModel.cs
@@ -7,5 +7,6 @@ public record LeilaoModel
     public int Relogio_id { get; set; } = 0;
     public string DataInicio { get; set; } = "";
     public string DataFim { get; set; } = "";
+    public int LicitacaoAtual { get; set; } = 0;
     public int Utilizador_idUtilizador { get; set; } = 0;
 }
