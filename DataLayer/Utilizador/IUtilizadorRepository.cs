04ca549fe184f2cb282c7c97df5e6ab1f9834ac4
Luís <102376495+jluis02@users.noreply.github.com>
2024-01-21 18:08:16 +0000

    update do num de leiloes

--- DataLayer/Utilizador/IUtilizadorRepository.cs
+++ DataLayer/Utilizador/IUtilizadorRepository.cs
@@ -6,6 +6,7 @@
         public Task<List<UtilizadorModel>> FindAll();
         public Task Create(UtilizadorModel card);
         public Task<UtilizadorModel> Update(UtilizadorModel user);
+        public Task<UtilizadorModel> UpdateNumLeilao(string user, int numLeilao);
         public Task Remove(int code);
     }
 }
